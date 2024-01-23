940bf125b6eb02b4676b4ec07dcc9d102551c687
zxtas <117494428+zxtas@users.noreply.github.com>
2024-01-23 14:08:15 -0800

    Functionality!

--- bot.py
+++ bot.py
@@ -2,18 +2,27 @@ import os
 import discord
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
+import asyncio
 
 load_dotenv()
 
 token = os.getenv('token')
 
-intents = discord.Intents.All()
+intents = discord.Intents.all()
 
-client = commands.Bot(command_Prefix=";", intents = intents)
+client = commands.Bot(command_prefix=";", intents = intents)
 
 @client.event
-async def ready():
+async def on_ready():
     print(f"Success: {client.user} Online.")
 
+@client.command(aliases=["ping", 'ms'])
+async def hey(ctx):
+    await ctx.send("{0}ms." .format(round(client.latency * 1000)))
 
+async def main():
+    async with client:
+       # await load()
+        await client.start(token)
 
+asyncio.run(main())
