f420f84b7866dd7d358b58b6ffe374c56b18bc0c
zxtas <117494428+zxtas@users.noreply.github.com>
2024-06-27 11:36:29 -0700

    LMFAO ignore this

--- src/cogs/roi.py
+++ src/cogs/roi.py
@@ -33,6 +33,48 @@ class roi(commands.Cog):
             await ctx.send(f'<@275459983888613376> went afk for the {ordinal(number)} time, Last Afk {lastafk}')
             writenumber.close
 
+    
+    @commands.command()
+    async def fire(self, ctx):
+        async with ctx.typing():
+            await ctx.send("http://www.glitchingqueen.com/uploads/8/7/2/1/87212810/waterfire_1_orig.png")
+
+    @commands.command()
+    async def ice(self, ctx):
+        async with ctx.typing():
+            await ctx.send("http://www.glitchingqueen.com/uploads/8/7/2/1/87212810/fixed-ice-staff_1_orig.png")
+
+    @commands.command()
+    async def wind(self, ctx):
+        async with ctx.typing():
+            await ctx.send("http://www.glitchingqueen.com/uploads/8/7/2/1/87212810/waterwind_3_orig.png")
+            
+    @commands.command()
+    async def light(self, ctx):
+        async with ctx.typing():
+            await ctx.send("http://www.glitchingqueen.com/uploads/8/7/2/1/87212810/waterlightning_orig.png")
+
+    @commands.command()
+    async def easter(self, ctx):
+        async with ctx.typing():
+            await ctx.send("http://www.glitchingqueen.com/uploads/8/7/2/1/87212810/finalorigee_1_orig.png")
+            
+    @commands.command()
+    async def shield(self, ctx):
+        async with ctx.typing():
+            await ctx.send("http://www.glitchingqueen.com/uploads/8/7/2/1/87212810/shieldd_orig.jpg")
+
+    @commands.command()
+    async def freemagma(self, ctx):
+        async with ctx.typing():
+            await ctx.send("http://www.glitchingqueen.com/uploads/8/7/2/1/87212810/m8o8_orig.jpg")
+            
+    @commands.command()
+    async def reddig(self, ctx):
+        async with ctx.typing():
+            await ctx.send("http://www.glitchingqueen.com/uploads/8/7/2/1/87212810/smaller-dig-steps_1_orig.png")
+
+
 
 
 async def setup(client):
